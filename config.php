603d0e0b65587205778c4397df3ddc1cb6195a4c
Amoo Pedram <Pedroxam@gmail.com>
2019-05-01 18:14:49 -0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+/*=====================
+* Config File For FFmpeg Tile Thumbnailer
+* By Pedram Asbaghi
+/*=====================*/
+
+error_reporting(E_ALL);
+
+define('BASE', dirname(__FILE__));
+
+define('STORAGE', '/store/');
+
+define('URI', '.'); //!
